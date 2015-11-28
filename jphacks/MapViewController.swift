760e4cae305683e39a1aa3d0969cf0642a7496ab
ha1fha1f <half.ldk.hiz@gmail.com>
2015-11-28 11:48:27 +0900

    ルート表示機能

--- jphacks/MapViewController.swift
+++ jphacks/MapViewController.swift
@@ -16,7 +16,7 @@ class MapViewController: UIViewController {
         
     }
     
-    let mapView = MKMapView()
+    private let mapView = MKMapView()
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -34,7 +34,9 @@ class MapViewController: UIViewController {
             self.view.addSubview(mapView)
         }
         mapView.delegate = self
-        mapView.setRegion(MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(37.506804, 139.930531), 100, 100), animated: true)
+        mapView.setRegion(MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake(37.506804, 139.930531), 1000, 1000), animated: true)
+        
+        addRoute(CLLocationCoordinate2D(latitude:34.6944022737767, longitude: 135.195888597644), toCoordinate: CLLocationCoordinate2D(latitude: 34.709759, longitude: 135.248512))
     }
     
     override func didReceiveMemoryWarning() {
@@ -48,6 +50,48 @@ class MapViewController: UIViewController {
         pin.title = "title"
         pin.subtitle = "sub title"
         mapView.addAnnotation(pin)
+    }
+    
+    func addRoute(fromCoordinate: CLLocationCoordinate2D, toCoordinate: CLLocationCoordinate2D) {
+        let fromItem: MKMapItem = MKMapItem(placemark: MKPlacemark(coordinate: fromCoordinate, addressDictionary: nil))
+        let toItem: MKMapItem = MKMapItem(placemark: MKPlacemark(coordinate: toCoordinate, addressDictionary: nil))
+        
+        // MKDirectionsRequestを生成.
+        let myRequest: MKDirectionsRequest = MKDirectionsRequest()
+        
+        // 出発&目的地
+        myRequest.source = fromItem
+        myRequest.destination = toItem
+        
+        myRequest.requestsAlternateRoutes = false
+        
+        // 徒歩
+        myRequest.transportType = MKDirectionsTransportType.Walking
+        
+        // MKDirectionsを生成してRequestをセット.
+        let myDirections: MKDirections = MKDirections(request: myRequest)
+        
+        // 経路探索.
+        myDirections.calculateDirectionsWithCompletionHandler { (response: MKDirectionsResponse?, error: NSError?) -> Void in
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            if let route = response?.routes.first as MKRoute? {
+                print("目的地まで \(route.distance)km")
+                print("所要時間 \(Int(route.expectedTravelTime/60))分")
+            
+                // mapViewにルートを描画.
+                self.mapView.addOverlay(route.polyline)
+            }
+
+        }
+    }
+    
+    // tapをおいたとき用
+    func getLocationFromTap(sender: UILongPressGestureRecognizer) -> CLLocationCoordinate2D {
+        return self.mapView.convertPoint(sender.locationInView(mapView), toCoordinateFromView: mapView)
     }
 }
 
@@ -72,7 +116,12 @@ extension MapViewController: CLLocationManagerDelegate {
     
     // 位置情報がupdateされた時
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        mapView.setRegion(MKCoordinateRegionMakeWithDistance(CLLocationCoordinate2DMake((manager.location?.coordinate.latitude)!, (manager.location?.coordinate.longitude)!), 100, 100), animated: true)
+        // 中心を変更
+        mapView.setCenterCoordinate(CLLocationCoordinate2DMake((manager.location?.coordinate.latitude)!, (manager.location?.coordinate.longitude)!), animated: true)
+        
+        // test, add pin
+        addPin((manager.location?.coordinate.latitude)!, lon: (manager.location?.coordinate.longitude)!)
+        print("\(manager.location?.coordinate.latitude),\(manager.location?.coordinate.longitude)")
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
@@ -84,5 +133,22 @@ extension MapViewController: MKMapViewDelegate {
     // Regionが変更された時に呼び出されるメソッド.
     func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
         print("regionDidChangeAnimated")
+    }
+    
+    // 経路を描画するときの色や線の太さを指定
+    
+    func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKPolyline {
+            let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 3
+            return polylineRenderer
+        } else {
+            // ダミー
+            let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 5
+            return polylineRenderer
+        }
     }
 }
