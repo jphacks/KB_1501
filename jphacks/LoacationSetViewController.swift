ff35f2fd656310ea26607fb400c3f32288ceca53
SakuragiYoshimasa <ysrhsp@outlook.com>
2015-11-28 10:46:45 +0900

    画面とLocationClass

---
+++ jphacks/LoacationSetViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoacationSetViewController.swift
+//  jphacks
+//
+//  Created by SakuragiYoshimasa on 2015/11/28.
+//  Copyright © 2015年 at. All rights reserved.
+//
+import UIKit
+
+protocol LocationSetterDelegate {
+    func 
+}
+
+class LocationSetViewController: UIViewController {
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ jphacks/Location.swift
@@ -0,0 +1,15 @@
+//
+//  Location.swift
+//  jphacks
+//
+//  Created by SakuragiYoshimasa on 2015/11/28.
+//  Copyright © 2015年 at. All rights reserved.
+//
+
+import Foundation
+
+class Location {
+    var latitude: Float = 0
+    var longitude: Float = 0
+    var locationName: String = ""
+}
