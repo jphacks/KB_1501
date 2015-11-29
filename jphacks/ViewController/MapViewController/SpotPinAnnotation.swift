fdbe09d84c94f23f34e984116bb3cf7f77e5829c
ha1fha1f <half.ldk.hiz@gmail.com>
2015-11-29 13:25:00 +0900

    受け取れた！！！

---
+++ jphacks/ViewController/MapViewController/SpotPinAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  SpotPinAnnotation.swift
+//  jphacks
+//
+//  Created by 山口智生 on 2015/11/29.
+//  Copyright © 2015年 at. All rights reserved.
+//
+
+import MapKit
+
+class SpotPinAnnotation: MKPointAnnotation {
+    var spot: Spot! = nil
+}
